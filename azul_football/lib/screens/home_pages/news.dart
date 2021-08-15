62b39d8cce9bdff1faf138c93a704e47b8196599
aakashsh1999 <aakash.sh858791@gmail.com>
2021-08-15 12:36:28 +0530

    api function added

--- azul_football/lib/screens/home_pages/news.dart
+++ azul_football/lib/screens/home_pages/news.dart
@@ -12,7 +12,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-
+import 'package:http/http.dart' as http;
 import 'bottom_nav_screen.dart';
 
 class NewsPage extends StatefulWidget {
