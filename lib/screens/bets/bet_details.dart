aa6ce02c795484ebbda10e6b755efa57f5282c17
nitin-mahour <nitin.mahour6@gmail.com>
2021-08-27 12:16:10 +0530

    made tab views

--- lib/screens/bets/bet_details.dart
+++ lib/screens/bets/bet_details.dart
@@ -11,16 +11,56 @@ class BetDetails extends StatefulWidget {
 
 class _BetDetailsState extends State<BetDetails> {
   void initState() {
-    print(widget.matchData);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(widget.matchData);
-    return SingleChildScrollView(
-      child: Container(color: Colors.grey,
-      child: Text(widget.matchData.toString()),),
+    final theme = Theme.of(context);
+
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            indicatorColor: theme.primaryColor,
+            unselectedLabelColor: theme.primaryColor,
+            labelColor: theme.primaryColor,
+            labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            tabs: [
+              Tab(
+                text: 'Top Bids',
+              ),
+              Tab(
+                text: 'Market Rate',
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 400,
+            child: TabBarView(children: [
+              Text(widget.matchData['marketRateInfo'].toString()),
+              Text(widget.matchData['marketRate'].toString()),
+            ]),
+          ),
+          Text(widget.matchData.toString()),
+        ],
+      ),
+      // child: Column(
+      //   children: [
+      //     Text(
+      //       'Market Rate',
+      //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      //       textAlign: TextAlign.center,
+      //     ),
+      //     TabBarView(
+      //       children: [
+      //         Text(widget.matchData.toString()),
+      //         Text(widget.matchData.toString()),
+      //       ],
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
